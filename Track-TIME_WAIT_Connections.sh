ff56c8a4e84cc5a0b327461994b4b450adabcc07
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-08-31 18:45:10 +0530

    final changes

--- Track-TIME_WAIT_Connections.sh
+++ Track-TIME_WAIT_Connections.sh
@@ -1,35 +1,41 @@
 # /bin/bash
 
 
-
-
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> TrackTW.txt
 echo "		$(date)"  >> TrackTW.txt
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> TrackTW.txt 
-echo "			TRACK TIME_WAIT AND ESTABLISHED CONNECTIONS"  >> TrackTW.txt
+echo "		TRACK TIME_WAIT AND ESTABLISHED CONNECTIONS"  >> TrackTW.txt
 echo "------------------------------------------------------------------------------" >> TrackTW.txt 
 echo "Proto Recv-Q Send-Q Local Address           Foreign Address         State "     >> TrackTW.txt
 echo "------------------------------------------------------------------------------" >> TrackTW.txt
 
+#get records into the text file
+netstat >> Track_TW.txt
 
-#netstat >> Track_TW.txt
+#count Time_Wait and Established connections.
 countTWconn=$(cat Track_TW.txt | grep "TIME_WAIT" | wc -l)
 countEstablishedconn=$(cat Track_TW.txt | grep "ESTABLISHED" | wc -l)
-#echo "$countTWconn"
-TWconnctions=2
-if [ "$countTWconn" -ge "$TWconnctions" ] 
+
+
+#set Config_threshold 
+Config_threshold=2
+
+#check TimeWait connections with config. threshold
+if [ "$countTWconn" -ge "$Config_threshold" ] 
 then
- echo "$(cat Track_TW.txt | grep "TIME_WAIT" )"  >> TrackTW.txt
-# echo "" >>TrackTW.txt
+ echo "$(cat Track_TW.txt | grep "TIME_WAIT" )"  >> TrackTW.txt 
  echo "------------------------------------------------------------------------------" >> TrackTW.txt
- echo "$(cat Track_TW.txt | grep "ESTABLISHED" )"  >> TrackTW.txt
+ echo "$(cat Track_TW.txt | grep "ESTABLISHED" )" >> TrackTW.txt
  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> TrackTW.txt
+ #get total count of TIME_WAIT and ESTABLISHED Connections. 
  echo "Total no of TIME_WAIT Connection: $countTWconn" >> TrackTW.txt
  echo "Total no of ESTABLISHED Connection: $countEstablishedconn" >> TrackTW.txt
  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> TrackTW.txt
-
+ 
 else 
+ #if there is no TIME_WAIT connections. 
  echo "There is no TIME_WAIT connections  "   >> TrackTW.txt
+ echo "All the connections are ESTABLISHED Successfully  ._. "   >> TrackTW.txt
  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> TrackTW.txt
 fi
-      
+ echo "Done"     
